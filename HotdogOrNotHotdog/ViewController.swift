344e1c5e794e18e8d79f30443514240ed64226f9
Kewal Patel <kaakaa135@gmail.com>
2021-12-01 17:54:02 -0500

    Initial Commit

---
+++ HotdogOrNotHotdog/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HotdogOrNotHotdog
+//
+//  Created by Kewal Patel on 12/1/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
